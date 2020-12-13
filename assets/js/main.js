1dbdc6942d1478d5b4527e9642acb49dca67df3c
Isa Wierum <iswierum@davidson.edu>
2020-12-13 04:56:44 -0600

    late night

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,51 @@
 
 $("#two-faced-button").on("click",function(){
 	$(".two-faced-popup").removeClass("hidden");
+	$(".two-faced-popup").addClass("visible");
+	$("#two-faced-button").addClass("hidden");
 });
+
+
+
+$(".x").on("click", function(){
+	$(".two-faced-popup").removeClass("visible");
+	$(".two-faced-popup").addClass("hidden");
+	$(".-results-popup").removeClass("hidden");
+
+});
+
+$(".upgrade").on("click", function(){
+	$(".two-faced-popup").removeClass("visible");
+	$(".two-faced-popup").addClass("hidden");
+	$(".-results-popup").removeClass("hidden");
+
+});
+
+
+$(".ok").on("click", function(){
+	$(".two-faced-popup").removeClass("visible");
+	$(".two-faced-popup").addClass("hidden");
+	$(".-results-popup").removeClass("hidden");
+
+});
+
+$(".solution1").on("click", function(){
+	$(".two-faced-popup").removeClass("visible");
+	$(".two-faced-popup").addClass("hidden");
+	$(".posi-results-popup").removeClass("hidden");
+});
+
+/*
+function hidden(classtohide) {
+  if($(classtohide.hasClass("visible")){
+		$("classtohide").removeClass("visible");
+		$("classtohide").addClass("hidden");
+	}
+	else {
+		$("classtohide").addClass("hidden");
+		$("classtohide").removeClass("visible");
+
+	}
+}
+
+*/
